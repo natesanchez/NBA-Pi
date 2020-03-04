2285c50f4419787bf3bfdb43e258322585036081
Nate_Sanchez <NSanchez@usa.com>
2020-03-04 16:59:06 -0500

    some changes

--- Index.js
+++ Index.js
@@ -19,18 +19,34 @@ app.get("/teams", function (req, res) {
     });
 });
 
+app.get("/teams/:team", function (req, res) {
+    NBA.find({}).then(nba => {
+        res.json(nba[0].teams.filter(item => req.params.team.toLowerCase() == item.name.toLowerCase())[0]);
+    });
+});
 
-app.get("/championships/", function (req, res) {
+
+app.get("/championships", function (req, res) {
     NBA.find({}).then(nba => {
         res.json(nba[0].championships);
     });
 });
 
 
-app.get("/legends/", function (req, res) {
+app.get("/legends", function (req, res) {
     NBA.find({}).then(nba => {
         res.json(nba[0].legends);
     });
+});
+
+app.get("/championships/:year", function (req, res) {
+    NBA.find({}).then(nba => {
+        res.json(nba[0].championships.filter(item => req.params.year == item.year)[0])
+    });
+});
+
+app.post("/legends", function (req, res) {
+    NBA.create(req.body).then(nba => res.json(nba.legends));
 });
 
 
