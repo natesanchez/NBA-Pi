252b00df51c567622bdf1c49e6498bf6d8b87ee2
Nate_Sanchez <NSanchez@usa.com>
2020-03-27 10:26:14 -0400

    api testing ignore

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const parser = require("body-parser");
-const Team = require("./lib/models/Team")
-const Champ = require("./lib/models/Champ")
-const Legend = require("./lib/models/Legend")
-const Player = require("./lib/models/Player")
+const Team = require("./lib/models/Team");
+const Champ = require("./lib/models/Champ");
+const Legend = require("./lib/models/Legend");
+const Player = require("./lib/models/Player");
 const cors = require("cors");
 
 app.use(parser.json());
@@ -12,15 +12,15 @@ app.use(parser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json({
-        Message: "Welcome to NBA-Pi!",
-        Documentation: "https://github.com/natesanchez/NBA-Pi",
-        Teams: "https://nba-pi.herokuapp.com/teams",
-        Championships: "https://nba-pi.herokuapp.com/championships",
-        Legends: "https://nba-pi.herokuapp.com/legends",
-        Players: "https://nba-pi.herokuapp.com/players"
-    })
-})
+  res.json({
+    Message: "Welcome to NBA-Pi!",
+    Documentation: "https://github.com/natesanchez/NBA-Pi",
+    Teams: "https://nba-pi.herokuapp.com/teams",
+    Championships: "https://nba-pi.herokuapp.com/championships",
+    Legends: "https://nba-pi.herokuapp.com/legends",
+    Players: "https://nba-pi.herokuapp.com/players"
+  });
+});
 
 // app.get("/", (req, res) => {
 //     res.json({
@@ -33,90 +33,82 @@ app.get("/", (req, res) => {
 //     })
 // })
 
-
-app.get("/teams", function (req, res) {
-    Team.find({}).then(teams => {
-        res.json(teams);
-    });
+app.get("/teams", function(req, res) {
+  Team.find({}).then(teams => {
+    res.json(teams);
+  });
 });
 
-
-app.get("/teams/:team", function (req, res) {
-    let input = req.params.team
-    input = input[0].toUpperCase() + input.substr(1);
-    Team.findOne({ name: input }).then(teams => {
-        res.json(teams);
-    });
+app.get("/teams/:team", function(req, res) {
+  let input = req.params.team;
+  input = input[0].toUpperCase() + input.substr(1);
+  Team.findOne({ name: input }).then(teams => {
+    res.json(teams);
+  });
 });
 
-
-app.get("/championships", function (req, res) {
-    Champ.find({}).then(champs => {
-        res.json(champs);
-    });
+app.get("/championships", function(req, res) {
+  Champ.find({}).then(champs => {
+    res.json(champs);
+  });
 });
 
-
-app.get("/championships/:year", function (req, res) {
-    let input = req.params.year
-    input = input[0].toUpperCase() + input.substr(1);
-    Champ.findOne({ year: input }).then(champs => {
-        res.json(champs);
-    });
+app.get("/championships/:year", function(req, res) {
+  let input = req.params.year;
+  input = input[0].toUpperCase() + input.substr(1);
+  Champ.findOne({ year: input }).then(champs => {
+    res.json(champs);
+  });
 });
 
-
-app.get("/legends", function (req, res) {
-    Legend.find({}).then(legends => {
-        res.json(legends);
-    });
+app.get("/legends", function(req, res) {
+  Legend.find({}).then(legends => {
+    res.json(legends);
+  });
 });
 
-
-app.get("/players", function (req, res) {
-    Player.find({}).then(players => {
-        res.json(players);
-    });
+app.get("/players", function(req, res) {
+  Player.find({}).then(players => {
+    res.json(players);
+  });
 });
 
-
-app.get("/players/:lastName", function (req, res) {
-    let input = req.params.lastName
-    input = input[0].toUpperCase() + input.substr(1);
-    Player.find({ lastName: input }).then(players => {
-        res.json(players);
-    });
+app.get("/players/:lastName", function(req, res) {
+  let input = req.params.lastName;
+  input = input[0].toUpperCase() + input.substr(1);
+  Player.find({ lastName: input }).then(players => {
+    res.json(players);
+  });
 });
 
-
-app.get("/players/team/:teamId", function (req, res) {
-    let input = req.params.teamId
-    input = input[0].toUpperCase() + input.substr(1);
-    Player.find({ teamId: input }).then(players => {
-        res.json(players);
-    });
+app.get("/players/team/:teamId", function(req, res) {
+  let input = req.params.teamId;
+  input = input[0].toUpperCase() + input.substr(1);
+  Player.find({ teamId: input }).then(players => {
+    res.json(players);
+  });
 });
 
-
-app.post("/legends", function (req, res) {
-    Legend.create(req.body).then(legends => res.json(legends));
+app.post("/legends", function(req, res) {
+  Legend.create(req.body).then(legends => res.json(legends));
 });
 
-
-app.post("/players", function (req, res) {
-    Player.create(req.body).then(players => res.json(players));
+app.post("/players", function(req, res) {
+  Player.create(req.body).then(players => res.json(players));
 });
 
+//new project
+app.get("/users12345user", function(req, res) {
+  User.find({}).then(users => {
+    res.json(users);
+  });
+});
 
 app.set("port", process.env.PORT || 4000);
 
-
 app.listen(app.get("port"), () => {
-    console.log(`✅ PORT: ${app.get("port")} 🌟`);
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
 });
-
-
-
 
 // app.listen(4000, () => {
 //     console.log("Server Running!")
